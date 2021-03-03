5392a2f0c190bcdd17103dd8ae216bd5b4e26542
ddvoryan <79960760+ddvoryan@users.noreply.github.com>
2021-03-03 12:15:37 -0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,6 +2,9 @@
 gameport=${gameport:-'27960'}
 rconport=${rconport:-'28960'}
 
+serverpass=${serverpass:-'private'}
+mappool=${mappool:-'mappool.txt'}
+
 location=${location:-'US Central'}
 name=${name:-"tBA* Turbo CA [$location]"}
 
@@ -17,6 +20,8 @@ stdbuf -oL -eL /home/${USER}/Steam/steamapps/common/Quake\ Live\ Dedicated\ Serv
     +set zmq_rcon_enable 1 \
     +set zmq_rcon_password "quake1337!" \
     +set zmq_rcon_port $rconport \
+    +set sv_privatePassword "$serverpass" \
+    +set sv_mapPoolFile "$mappool" \
     +set zmq_stats_enable 1 \
     +set zmq_stats_password "stats" \
     +set zmq_stats_port $gameport \
